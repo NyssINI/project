fb000dfe254eee4a2533b8d45907531e453740a0
NyssINI <igustingurahdenysyudiantara@gmail.com>
2026-04-06 17:06:11 +0800

    fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345678'),
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
